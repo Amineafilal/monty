25b6a127592f51317bec45a6d736ec39bc081bfa
Kamal <kamalmiftah01@gmail.com>
2024-01-18 17:28:57 +0100

    0x19. C - Stacks, Queues - LIFO, FIFO, fixing task 4

--- execute2.c
+++ execute2.c
@@ -31,6 +31,7 @@ void add(stack_t **stack,unsigned int ligne)
 	if (len < 2)
 	{
 		dprintf(2, "L%d: can't add, stack too short\n", ligne);
+		free(buf);
 		exit(EXIT_FAILURE);
 	}
 	element1 = (*stack)->n;
--- tokenize.c
+++ tokenize.c
@@ -7,7 +7,6 @@ void tokenize(char *buf, int ligne, stack_t **stack)
 	char *token;
 
 	token = strtok(buf, " \n\t");
-	if (token == NULL)
-		return;
-	check(token, ligne)(stack, ligne);
+	if (token != NULL && token[0] != '#')
+		check(token, ligne)(stack, ligne);
 }
